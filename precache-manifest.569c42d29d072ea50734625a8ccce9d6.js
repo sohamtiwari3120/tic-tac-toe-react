03d988e4250a34244dbe80ee952a0277420f56f9
Soham Tiwari <soham.tiwari800@gmail.com>
2020-03-18 15:46:28 +0530

    Updates

---
+++ precache-manifest.569c42d29d072ea50734625a8ccce9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8f8b96cd0422403e6da9640fff6476",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "ae47bdd688b42f68d89a",
+    "url": "/tic-tac-toe-react/static/css/main.9d6db2cf.chunk.css"
+  },
+  {
+    "revision": "bc443960dc145110e9a4",
+    "url": "/tic-tac-toe-react/static/js/2.85577530.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-toe-react/static/js/2.85577530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae47bdd688b42f68d89a",
+    "url": "/tic-tac-toe-react/static/js/main.63eefae2.chunk.js"
+  },
+  {
+    "revision": "23a0b97a8a870de180dd",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.474bc43c.js"
+  }
+]);
